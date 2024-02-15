1840feae6509d95468b495ecaebfd8b186b15e24
DarioLoForti <lofortidario@gmail.com>
2024-02-15 17:18:30 +0100

    popolo tabella tramite seeder

--- database/migrations/2024_02_15_142532_create_trains_table.php
+++ database/migrations/2024_02_15_142532_create_trains_table.php
@@ -18,9 +18,9 @@ return new class extends Migration
             $table->string('agency', 150);
             $table->string('departure_station', 150);
             $table->string('arrival_station', 150);
-            $table->float('departure_time', 4, 2);
-            $table->float('arrival_time', 4, 2);
-            $table->tinyInteger('train_code')->unsigned();
+            $table->dateTime('departure_time', 4, 2);
+            $table->dateTime('arrival_time', 4, 2);
+            $table->bigInteger('train_code')->unsigned();
             $table->tinyInteger('number_carriages')->unsigned();
             $table->boolean('in_time');
             $table->boolean('deleted');
--- database/seeders/UpdateTrainsTable.php
+++ database/seeders/UpdateTrainsTable.php
@@ -19,17 +19,22 @@ class UpdateTrainsTable extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i > 20; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             $new_train = new Train();
             $new_train->agency = $faker->company();
             $new_train->departure_station = $faker->city();
             $new_train->arrival_station = $faker->city();
-            $new_train->departure_time = $faker->numberFloat(2, 1, 24);
-            $new_train->arrival_time = $faker->numberFloat(2, 1, 24);
+            $new_train->arrival_time = $faker->dateTime();
             $new_train->train_code = $faker->numberBetween(1, 999999);
             $new_train->number_carriages = $faker->numberBetween(1, 10);
             $new_train->in_time = $faker->boolean();
             $new_train->deleted = $faker->boolean();
+            if ($i < 2) {
+                $new_train->departure_time = date('Y-m-d h:i:s');
+            } else {
+                $new_train->departure_time = $faker->dateTime();
+            }
+
             $new_train->save();
         }
     }

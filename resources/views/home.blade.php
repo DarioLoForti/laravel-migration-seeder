cff6bc91445383e7312839c9f1d24db5aa1dfb53
DarioLoForti <lofortidario@gmail.com>
2024-02-15 15:24:14 +0100

    creo layaut base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layaut.app')
+
+@section('content')
+@endsection
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Hello</h1>
+        </div>
+    </div>
+
+</div>

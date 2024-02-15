3c084786e2652ef04b109a445699e0c1104a0482
DarioLoForti <lofortidario@gmail.com>
2024-02-15 17:53:45 +0100

    creo filtro per partenza odierna dei treni nel controller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use DateTime;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $now = new DateTime(); // salvo la data odierna in una variabile
+        $today = $now->format('Y-m-d'); // formatto la data odierna in una stringa e la salvo nella variabile $today
+
+        $trains = Train::where('departure_time', 'like', $today . '%')->orderBy('departure_time', 'asc')->get();
 
         return view('home', compact('trains'));
     }
--- database/seeders/UpdateTrainsTable.php
+++ database/seeders/UpdateTrainsTable.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Faker\Provider\en_US\Address;
 use Faker\Provider\en_US\Company;
+use DataTime;
 
 use App\Models\Train;
 
@@ -19,12 +20,14 @@ class UpdateTrainsTable extends Seeder
      */
     public function run(Faker $faker)
     {
+        $today = new DateTime();
+
         for ($i = 0; $i < 20; $i++) {
             $new_train = new Train();
             $new_train->agency = $faker->company();
             $new_train->departure_station = $faker->city();
             $new_train->arrival_station = $faker->city();
-            $new_train->arrival_time = $faker->dateTime();
+            $new_train->arrival_time = $faker->dateTimeBetween($today, '+10 days');
             $new_train->train_code = $faker->numberBetween(1, 999999);
             $new_train->number_carriages = $faker->numberBetween(1, 10);
             $new_train->in_time = $faker->boolean();
@@ -32,7 +35,7 @@ class UpdateTrainsTable extends Seeder
             if ($i < 2) {
                 $new_train->departure_time = date('Y-m-d h:i:s');
             } else {
-                $new_train->departure_time = $faker->dateTime();
+                $new_train->departure_time = $faker->dateTimeBetween($today, '+10 days');
             }
 
             $new_train->save();
